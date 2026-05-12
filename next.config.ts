57974b136001081ab9791b00df234a4991221926
VitorMF <vitorfreire419@gmail.com>
2026-05-12 19:46:52 -0300

    fix: add domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // permite acessar via IP da rede local em dev
-  allowedDevOrigins: ["192.168.1.14", "localhost"],
+  allowedDevOrigins: ["192.168.1.14", "localhost", "postou.vercel.app"],
 };
 
 export default nextConfig;
