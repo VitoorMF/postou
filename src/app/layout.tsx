0fffc9b82324fdc6e02687c3b4c57ce0e33479fb
VitorMF <vitorfreire419@gmail.com>
2026-06-25 18:50:56 -0300

    feat: vercel analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,7 @@ export default function RootLayout({
     >
       <body className="bg-black text-white">
         {children}
+        <Analytics />
       </body>
     </html>
   );
